02bc9d6ae4dae84396c57651437f89d8864d02c4
ztrkosmn09 <ztrkosmn09@gmail.com>
2022-12-26 22:36:20 +0100

    server wurde überarbeitet

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 
 
-app.use('/',postRoutes)
+app.use('/posts',postRoutes)
 
 
 app.listen(PORT,()=>{
