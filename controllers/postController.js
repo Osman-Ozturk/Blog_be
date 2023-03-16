3eb0554cdd443a71e4fde9dea8852d449814f39a
ztrkosmn09 <ztrkosmn09@gmail.com>
2023-03-16 15:03:38 +0100

    edited

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,6 +10,18 @@ const getPosts = async (req, res, next) => {
     next(error);
   }
 };
+
+const getSinglePosts = async (req, res, next) => {
+  try {
+    const {id:_id}=req.params
+    const post = await Post.findById(_id);
+    
+      res.status(201).json(post);
+   
+  } catch (error) {
+    next(error);
+  }
+};
 const createPost = async (req, res, next) => {
   try {
     const post = await Post.create(req.body)
@@ -18,5 +30,23 @@ const createPost = async (req, res, next) => {
     next(error);
   }
 };
+const deletePost = async (req, res, next) => {
+  try {
+    const {id:_id}=req.params
+    const post = await Post.findByIdAndDelete(_id)
+  } catch (error) {
+    next(error);
+  }
+};
 
-export { getPosts, createPost };
+
+const updatePost = async (req, res, next) => {
+  try {
+    const {id:_id}=req.params
+    const post = await Post.findByIdAndUpdate(_id,req.body,{new:true})
+    res.status(201).json(post)
+  } catch (error) {
+    next(error);
+  }
+};
+export { getPosts, createPost ,getSinglePosts,deletePost,updatePost};
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {getPosts ,createPost} from '../controllers/postController.js'
+import {getPosts ,createPost,getSinglePosts,deletePost,updatePost} from '../controllers/postController.js'
 const router = express.Router()
 
 router.route('/').get(getPosts).post(createPost)
+router.route('/:id').get(getSinglePosts).delete(deletePost).patch(updatePost)
 
 
 export default router
